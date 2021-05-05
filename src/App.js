695fc7917759de07c3e55a476395a0e3e90b469e
Travers Pinkerton <jtpinkerton@gmail.com>
2021-05-05 12:36:59 -0500

    Add error handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import BreedList from "./components/BreedList";
 import SearchInput from "./components/SearchInput";
@@ -6,14 +6,22 @@ import SearchInput from "./components/SearchInput";
 function App() {
   const [breeds, setBreeds] = useState();
   const [searchTerm, setSearchTerm] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
 
   useEffect(() => {
     async function getBreeds() {
-      const resp = await fetch("https://dog.ceo/api/breeds/list/all");
-      const data = await resp.json();
-      if (data.message) {
-        const breedList = Object.keys(data.message);
-        setBreeds(breedList);
+      try {
+        const resp = await fetch("https://dog.ceo/api/breeds/list/all");
+        const data = await resp.json();
+        if (data.message) {
+          const breedList = Object.keys(data.message);
+          setBreeds(breedList);
+          setErrorMsg();
+        } else {
+          setErrorMsg("Oops. Something went wrong");
+        }
+      } catch (error) {
+        setErrorMsg("Oops. Something went wrong");
       }
     }
 
@@ -28,11 +36,22 @@ function App() {
     <div>
       <div className="app">
         <h1>Good boy</h1>
-        <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <div className="breedlist-container">
-          {!breeds && <h2>Fetching some good boys!</h2>}
-          {!!breeds && <BreedList breeds={filteredBreeds} />}
-        </div>
+        {!!errorMsg ? (
+          <h2>{errorMsg}</h2>
+        ) : (
+          <React.Fragment>
+            <SearchInput
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+            />
+            <div className="breedlist-container">
+              {!breeds && <h2>Fetching some good boys!</h2>}
+              {!!breeds && (
+                <BreedList breeds={filteredBreeds} setErrorMsg={setErrorMsg} />
+              )}
+            </div>
+          </React.Fragment>
+        )}
       </div>
     </div>
   );
--- src/components/BreedList.js
+++ src/components/BreedList.js
@@ -1,30 +1,43 @@
 import { useEffect, useState } from "react";
 import "../styles/BreedList.css";
 
-function BreedList({ breeds }) {
+function BreedList({ breeds, setErrorMsg }) {
   return (
     <div className="breedlist">
       {breeds.map((breedName) => (
-        <BreedListItem key={breedName} breedName={breedName} />
+        <BreedListItem
+          key={breedName}
+          breedName={breedName}
+          setErrorMsg={setErrorMsg}
+        />
       ))}
     </div>
   );
 }
 
-function BreedListItem({ breedName }) {
+function BreedListItem({ breedName, setErrorMsg }) {
   const [imgUrl, setImgUrl] = useState();
 
   useEffect(() => {
     async function fetchBreedData() {
-      const resp = await fetch(
-        `https://dog.ceo/api/breed/${breedName}/images/random`
-      );
-      const data = await resp.json();
-      if (data.status === "success") setImgUrl(data.message);
+      try {
+        const resp = await fetch(
+          `https://dog.ceo/api/breed/${breedName}/images/random`
+        );
+        const data = await resp.json();
+        if (data.status === "success") {
+          setImgUrl(data.message);
+          setErrorMsg();
+        } else {
+          setErrorMsg("Oops. Something went wrong");
+        }
+      } catch (error) {
+        setErrorMsg("Oops. Something went wrong");
+      }
     }
 
     fetchBreedData();
-  }, [breedName]);
+  }, [breedName, setErrorMsg]);
 
   if (!imgUrl) return <div>{breedName}</div>;
   return (
